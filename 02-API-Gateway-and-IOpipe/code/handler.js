875f116971c51145b5119c72f6d81e964b422dfa
Mx. Kas Perch <the@nodebotani.st>
2018-04-30 15:20:35 -0500

    code for chapter 3

--- 02-API-Gateway-and-IOpipe/code/handler.js
+++ 02-API-Gateway-and-IOpipe/code/handler.js
@@ -1,16 +1,19 @@
-'use strict';
+'use strict'
 
-module.exports.hello = (event, context, callback) => {
+const IOpipelib = require('@iopipe/iopipe')
+
+iopipe = IOpipelib({
+  token: ACCESS_TOKEN
+})
+
+module.exports.hello = iopipe((event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+      input: event
+    })
+  }
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  callback(null, response)
+})
